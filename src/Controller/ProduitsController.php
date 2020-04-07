3362e52eb1fc62b43f9e8e1ea8d2930d7753e14b
Gashmob <kevintraini73@outlook.fr>
2020-04-07 13:11:04 +0200

    CSS de base de la page show_vessel

--- src/Controller/ProduitsController.php
+++ src/Controller/ProduitsController.php
@@ -19,8 +19,22 @@ class ProduitsController extends AbstractController
     public function home()
     {
         $vaisseaux = [
-            ['nom' => 'test', 'image' => '/images/Hediata.png', 'prix' => 3000, 'description' => 'Logo de la faction'],
-            ['nom' => 'flotte_de_100_chasseurs', 'image' => '/images/fond_header.jpg', 'prix' => 1000000, 'description' => 'Très pratique pour attaquer des personnes'],
+            [
+                'id' => 1,
+                'nom' => 'flotte_de_100_vaisseaux',
+                'prix' => 150450,
+                'description' => 'Il s\'agit d\'une flotte de 100 chasseurs bien armés, avec toutes leurs munitions.',
+                'image' => '/images/fond_header.jpg',
+                'date_creation' => '2020-02-25',
+            ],
+            [
+                'id' => 2,
+                'nom' => 'cuirassé_en_fin_de_vie',
+                'prix' => 300,
+                'description' => 'Ce cuirassé a déjà servi pour plusieurs batailles. Il a dû être rafistolé dans tous les sens, mais il tient bien. Malheureusement les munitions pour son armement ne sont plus produites car obsolètes.',
+                'image' => '/images/Hediata.png',
+                'date_creation' => '2020-02-25',
+            ],
         ];
 
         return $this->render('produits/produits.html.twig', [
@@ -29,13 +43,21 @@ class ProduitsController extends AbstractController
     }
 
     /**
-     * @Route("/vessel/{name}", name="app_show_vessel")
+     * @Route("/vessel/{id}", name="app_show_vessel")
      */
-    public function showVessel($name)
+    public function showVessel($id)
     {
+        $vaisseau = [
+            'id' => $id,
+            'nom' => 'cuirassé_en_fin_de_vie',
+            'prix' => 300,
+            'description' => 'Ce cuirassé a déjà servi pour plusieurs batailles. Il a dû être rafistolé dans tous les sens, mais il tient bien. Malheureusement les munitions pour son armement ne sont plus produites car obsolètes.',
+            'image' => '/images/Hediata.png',
+            'date_creation' => '2020-02-25',
+        ];
 
         return $this->render('produits/show_vessel.html.twig', [
-            'name' => $name,
+            'vaisseau' => $vaisseau,
         ]);
     }
 }
