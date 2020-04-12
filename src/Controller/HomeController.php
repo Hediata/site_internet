985ffea4134a2687b8e528e55ce479aeaab19ce0
Gashmob <kevintraini73@outlook.fr>
2020-04-12 11:03:03 +0200

    Ajout de la connexion

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,12 +6,22 @@ namespace App\Controller;
 
 use App\Entity\Sections;
 use App\Entity\Utilisateurs;
+use App\Form\ConnexionFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
     /**
      * @Route("/", name="app_homepage")
      */
@@ -28,9 +38,41 @@ class HomeController extends AbstractController
     /**
      * @Route("/connexion", name="app_connexion")
      */
-    public function connexion()
+    public function connexion(EntityManagerInterface $em, Request $request)
     {
-        return $this->render('home/connexion.html.twig');
+        $form = $this->createForm(ConnexionFormType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $data = $form->getData();
+            $repositoryU = $em->getRepository(Utilisateurs::class);
+            $user = $repositoryU->findOneBy([
+                'login' => $data->getLogin(),
+                'mot_de_passe' => $data->getMotDePasse(),
+            ]);
+
+            if ($user)
+            {
+                $this->session->set('user', $user);
+                $this->addFlash('success', 'Vous êtes connecté(e) !');
+                return $this->redirectToRoute('app_user', ['login' => $user->getLogin()]);
+            }
+        }
+
+        return $this->render('home/connexion.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/deconnexion", name="app_deconnexion")
+     */
+    public function deconnexion()
+    {
+        $this->session->remove('user');
+
+        return $this->redirectToRoute('app_homepage');
     }
 
     /**
@@ -52,9 +94,15 @@ class HomeController extends AbstractController
 
         if ($login === "")
         {
-            $login = "gashmob";
+            if ($this->session->get('user'))
+            {
+                $user = $this->session->get('user');
+            }
         }
-        $user = $repositoryU->findOneBy(['login' => $login]);
+        else
+        {
+            $user = $repositoryU->findOneBy(['login' => $login]);
+        }
 
         if (!$user)
         {
